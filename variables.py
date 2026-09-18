b63efc755df7ca1ed422a186376452733c0c7bc0
Johann <johannmarticorena@gmail.com>
2026-09-18 11:41:23 -0600

    Commit 2: trabajando con variables

--- variables.py
+++ variables.py
@@ -1,2 +1,9 @@
 #En esta clase aprenderé sobre vriables
-print("Hola mundo")
+print("Sistema de mensajería")
+distancia_km = 40.7
+peso_kg = 2.0
+es_urgente = True
+zona = "B"
+
+distancia_km = distancia_km + 16
+print (distancia_km)
